4010d0830e851e4258e6324116b1aa34801d909f
Artur Romasiuk <artur.romasiuk@gmail.com>
2017-01-26 01:13:25 +0200

    First commit

---
+++ app/src/main/java/com/example/arutr/mynewapp/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.arutr.mynewapp;
+
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, ClipboardManager.OnPrimaryClipChangedListener {
+    private TextView copytext, pastetext;
+    private ClipboardManager myClipboard;
+    private ClipData myClip;
+    private Button btnCreate;
+    private LinearLayout lMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clipboard_history);
+        myClipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        copytext = (EditText) findViewById(R.id.editText1);
+        pastetext = (EditText) findViewById(R.id.editText2);
+        btnCreate = (Button) findViewById(R.id.btnCreate);
+        lMain = (LinearLayout) findViewById(R.id.lMain);
+        btnCreate.setOnClickListener(this);
+        myClipboard.addPrimaryClipChangedListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        TextView b = (TextView)v;
+        String text = b.getText().toString();
+        myClip = ClipData.newPlainText("text", text);
+        myClipboard.setPrimaryClip(myClip);
+        Toast.makeText(getApplicationContext(), "Text Copied",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPrimaryClipChanged() {
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams
+                (ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        int btnGravity = Gravity.LEFT;
+        ClipData cp = myClipboard.getPrimaryClip();
+        ClipData.Item item = cp.getItemAt(0);
+        String text = item.getText().toString();
+        layoutParams.gravity = btnGravity;
+        TextView tvNew = new TextView(this);
+        tvNew.setClickable(true);
+        tvNew.setOnClickListener(this);
+        tvNew.setText(text);
+        lMain.addView(tvNew, layoutParams);
+    }
+}
